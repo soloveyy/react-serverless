103205c4efad1a24d06ab27d14c62d1a9fa2731a
soloveyy <33604729+soloveyy@users.noreply.github.com>
2021-05-08 21:37:06 +0200

    change about component to project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import {
     Route,
     Link
   } from "react-router-dom";
-import About from '../pages/About';
+import Project from '../pages/Project';
 import Contact from '../pages/Contact';
 import Home from '../pages/Home'
 
@@ -15,13 +15,13 @@ export default function Navbar() {
             <div>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/project">Project</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
 
                 <Switch>
-                <Route path="/about">
-                    <About />
+                <Route path="/project">
+                    <Project />
                 </Route>
                 <Route path="/contact">
                     <Contact />
