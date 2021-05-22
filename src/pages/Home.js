76e174344428c6447db6e13ffafbe120f86ad768
soloveyy <33604729+soloveyy@users.noreply.github.com>
2021-05-22 19:22:12 +0200

    Add Action Button on Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {StartGameButton} from "../styled/StyledHome"
+import Game from './Game'
 
 export default function Home() {
     return (
         <div>
             <h1>Hello from Home</h1>
+            <StartGameButton to="/game">Start to playing!</StartGameButton>
         </div>
     )
 }
